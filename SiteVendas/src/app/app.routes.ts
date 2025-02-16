5e7ca219cc311cf57b2e07f2b774efa2a77b1e76
thiago-fernandes <thicodes.com>
2025-02-16 20:26:37 -0300

    Consumindo um serviço que te retorna dados

--- SiteVendas/src/app/app.routes.ts
+++ SiteVendas/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from "./navegacao/home/home.component";
 import { ContatoComponent } from "./instituicao/contato/contato.component";
 import { SobreComponent } from "./instituicao/sobre/sobre.component";
 import { DataBindingComponent } from "./demos/data-binding/data-binding.component";
+import { ListaProdutoComponent } from "./produtos/lista-produto/lista-produto.component";
 
 export const rootRouterConfig: Routes = [
    {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -10,7 +11,11 @@ export const rootRouterConfig: Routes = [
 
    {path: 'contato', component: ContatoComponent},
    { path: 'sobre', component: SobreComponent },
-   { path: 'feature-data-binding', component: DataBindingComponent}
+   { path: 'feature-data-binding', component: DataBindingComponent},
+
+   { path: 'produtos', component: ListaProdutoComponent },
+
+   { path: 'produto-detalhe/:id', component: ListaProdutoComponent }
 
 
 
